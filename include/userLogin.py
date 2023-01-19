548159d3ed78ebc88e444dbb79e019a0c8a2583c
Fabrício Barbosa Viegas <incy8498@gmail.com>
2023-01-18 21:10:56 -0300

    Update userLogin.py

--- include/userLogin.py
+++ include/userLogin.py
@@ -3,44 +3,23 @@ from nio import AsyncClient
 from flask import session
 from flask_session import Session
 
-def clientLogin(username='', password='', homeserver='', checkLogout = False):
-    if not session.get('access_token') and checkLogout == False:   
-        print('login')
-        client = AsyncClient(
-            homeserver, 
-            username
-        )
-        
-        response = asyncio.run(login(client, password))
-
-        return response
-    
-    elif session.get('access_token') and checkLogout == False:
-        print(session['homeserver'])
-        client = AsyncClient(session['homeserver'])
-
-        client.access_token = session["access_token"]
-        client.user_id = session["user_id"]
-        client.device_id = session["device_id"]
-
-        return response
-
-    elif checkLogout:
-        print('logout')
-        response = clientLogout(client)
-    
-        return response
 
 
+def clientLogin():
 
-async def login(client, password):
-    response = await client.login(password)
+    client = AsyncClient(session['homeserver'])
 
-    return response
+    client.access_token = session["access_token"]
+    client.user_id = session["user_id"]
+    client.device_id = session["device_id"]
 
+    asyncio.run(main(client))
 
+async def main(client):
+    response = True
 
-async def clientLogout(client):
-    response = await client.logout()
-    
+    if session.get('logout_status'):
+        response = await client.logout()
+        print(response)
+
     return response
