a128680b32b8f82774ef08fccbe41387b24b7c72
Fabrício Barbosa Viegas <incy8498@gmail.com>
2023-01-28 16:52:22 -0300

    Update utils.py

--- include/utils.py
+++ include/utils.py
@@ -1,23 +1,29 @@
 import requests
 from flask import session
-from flask_session import Session
-
 
 
 def getAvatar(userName):
-    print(session["homeserver"])
     iam = requests.get(f'{session["homeserver"]}/_matrix/client/v3/profile/{userName}')
     if 'avatar_url' in iam.json():
-        avatarData = iam.json()['avatar_url'].split('/')[-2:]
+        avatarData = iam.json()['avatar_url']
 
-        return f"https://matrix.onemessenger.tk/_matrix/media/r0/thumbnail/{avatarData[0]}/{avatarData[1]}?width=32&height=32"
+        return convertMXC(avatarData)
     
     else:
         return False
 
 
 
+def convertMXC(mxcUrl):
+    mxcData = mxcUrl.split('/')[-2:]
+    
+    return f"https://matrix.onemessenger.tk/_matrix/media/r0/thumbnail/{mxcData[0]}/{mxcData[1]}?width=32&height=32"
+
+
+
 def getJoinedRooms():
         joins = requests.get(f'{session["homeserver"]}/_matrix/client/v3/joined_rooms?access_token={session["access_token"]}')
-
-        return joins
+        if joins.status_code == 200:
+            return joins.json()['joined_rooms']
+        else:
+            return []
